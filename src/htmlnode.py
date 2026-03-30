c6a65e3a1c453294b1291992c253a252cbea6a39
Serious-Josh <chapman.rjosh@gmail.com>
2026-03-30 13:28:23 +0000

    Added more types of HTMLNodes (Leaf/Parent) and added conversion functionality from TextNodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,3 +23,38 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode: No value")
+        elif self.tag is None:
+            return self.value
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode: No tag")
+        elif self.children is None:
+            raise ValueError("ParentNode: No children")
+        
+        children = ""
+
+        for each in self.children:
+            children += each.to_html()
+        
+        return f"<{self.tag}>{children}</{self.tag}>" 
